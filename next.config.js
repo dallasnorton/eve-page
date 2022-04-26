c67dae87daff8d7c6395c47a2fae3f7af7e2bf30
Dallas Norton <d.norton@f5.com>
2022-04-26 11:26:14 -0600

    white loading spinner

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-// for dev branch, to work with eve nginx server the assetPrefix should be ''
-module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/eve-page/' : '',
-};
