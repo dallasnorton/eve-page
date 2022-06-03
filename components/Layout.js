cbc6854345676a2c18cfef566e6f9c576f8c3eed
Dallas Norton <d.norton@f5.com>
2022-06-03 12:05:11 -0600

    delete button

--- components/Layout.js
+++ components/Layout.js
@@ -102,6 +102,12 @@ export default function Main() {
     }
   };
 
+  const onDelete = async (index) => {
+    const linksCopy = [...links];
+    linksCopy.splice(index, 1);
+    setLinks(linksCopy);
+  };
+
   return (
     <div className="max-w-8xl mx-auto">
       <section className="w-full text-black">
@@ -165,13 +171,16 @@ export default function Main() {
                   </div>
                 </div>
                 {links.length > 0 && (
-                  <div className="flex flex-col">
+                  <div className="flex w-full flex-col">
                     <p className="text-md block font-medium leading-8 text-gray-700">
                       Active Apps
                     </p>
                     <div className="flex flex-col items-start">
                       {links.map((link, index) => (
-                        <div key={index} className="mt-2 flex h-6 items-center">
+                        <div
+                          key={index}
+                          className="mt-2 flex h-6 w-full items-center justify-between md:w-1/2"
+                        >
                           <a
                             href={link.ref}
                             target="_blank"
@@ -180,6 +189,7 @@ export default function Main() {
                             flex
                             items-center
                             justify-center
+                            truncate
                             text-lg
                             font-bold
                             text-blue-700
@@ -187,7 +197,7 @@ export default function Main() {
                             hover:text-blue-500
                           "
                           >
-                            {link.name}
+                            <div className="w-full truncate">{link.name}</div>
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               className="ml-1 h-4 w-4"
@@ -198,6 +208,25 @@ export default function Main() {
                               <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
                             </svg>
                           </a>
+                          <button
+                            className="ml-4 cursor-pointer text-blue-700 hover:text-blue-500"
+                            onClick={() => onDelete(index)}
+                          >
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              className="h-4 w-4"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                              strokeWidth="2"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                              />
+                            </svg>
+                          </button>
                         </div>
                       ))}
                     </div>
